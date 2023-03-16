d9d9bbdcc5c896e2aa561d8c95f311a4db4d017c
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-03-16 15:47:07 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import SignupView from "../views/SignupView.vue";
+import ProductsView from "../views/products/ProductsView.vue";
+import ProductDetailsView from "../views/products/ProductDetailsView.vue";
+import AboutView from "../views/AboutView.vue";
+import ContactView from "../views/ContactView.vue";
+import WishList from "../views/WishList.vue";
+import CartView from "../views/CartView.vue";
+import UserView from "../views/UserView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const routes = [
   {
@@ -8,13 +18,55 @@ const routes = [
     component: HomeView,
   },
   {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
+  },
+  {
+    path: "/products",
+    name: "products",
+    component: ProductsView,
+  },
+  {
+    path: "/products/:id",
+    name: "productDetails",
+    component: ProductDetailsView,
+  },
+  {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: ContactView,
+  },
+  {
+    path: "/wishlist",
+    name: "wishlist",
+    component: WishList,
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: UserView,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: CartView,
+  },
+  // catchall 404
+  {
+    path: "/:catchAll(.*)",
+    name: "notFound",
+    component: NotFoundView,
   },
 ];
 
