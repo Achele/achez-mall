3aa514d7210adc0ce52996b23d1caa32a57475ba
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-03-16 15:51:27 +0100

    Create WishList.vue

---
+++ src/views/WishList.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>Wishlist</h1>
+  </main>
+
+  <FooterView />
+</template>
+
+<script setup>
+import FooterView from "@/components/FooterView.vue";
+</script>
+
+<style scoped>
+main {
+  padding: 1em 2em;
+}
+</style>
