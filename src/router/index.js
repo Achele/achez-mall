0dcfb22b3323881e130bcc4b3538880467a13ecc
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-03-22 01:26:05 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,8 @@ import WishList from "../views/WishList.vue";
 import CartView from "../views/CartView.vue";
 import UserView from "../views/UserView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
+import store from "../store/index";
+// import { getAuth } from "firebase/auth";
 
 const routes = [
   {
@@ -21,16 +23,21 @@ const routes = [
     path: "/login",
     name: "login",
     component: LoginView,
+    meta: { reqiresAuth: false },
   },
   {
     path: "/signup",
     name: "signup",
     component: SignupView,
+    meta: { requiresAuth: false },
   },
   {
     path: "/products",
     name: "products",
     component: ProductsView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/products/:id",
@@ -73,6 +80,29 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (
+    "requiresAuth" in to.meta &&
+    to.meta.requiresAuth &&
+    store.state.authIsReady !== true
+  ) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    next({ name: "login" });
+    // console.log(store.state.user);
+  } else if (
+    "requiresAuth" in to.meta &&
+    !to.meta.requiresAuth &&
+    store.state.authIsReady
+  ) {
+    next({ name: "products" });
+    // console.log(store.state.authIsReady);
+  } else {
+    next();
+    // console.log(store.state.authIsReady);
+  }
 });
 
 export default router;
